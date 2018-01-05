330d9247bf26eb17e013d39e0c9ccd08a6de9a06
Cam Nguyen <cnguyen1187@gmail.com>
2018-01-05 03:55:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,6 @@
 def prime?(number)
   number = number.abs
   return false if number <= 1
-return false if number % n == 0
+  (2..number-1).each {|n| return false if number % n == 0}
   return true
 end
